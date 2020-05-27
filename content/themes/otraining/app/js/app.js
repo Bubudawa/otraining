b7252b5bf26a5d698923368f36decb730ee96b69
Bubudawa <sylvainbuignet@gmail.com>
2020-05-27 17:06:01 +0200

    integration + media queries + JS

--- content/themes/otraining/app/js/app.js
+++ content/themes/otraining/app/js/app.js
@@ -1,6 +1,34 @@
-var app = {
+ var app = {
   init: function() {
-    console.log('init');
+
+
+
+    const openModal = document.querySelector('.fa-bars');
+    openModal.addEventListener('click', app.handleOpenMenu);
+
+    const closeModal = document.querySelector('.fa-close');
+    closeModal.addEventListener('click', app.handleCloseMenu);
+
+    const closeModalByWrapper = document.querySelector('.wrapper');
+    closeModalByWrapper.addEventListener('click', app.handleCloseMenu);
+  },
+
+  handleOpenMenu: function(){
+    const seeMenuBurger = document.querySelector('.menu-burger');
+    seeMenuBurger.classList.add('fs');
+    const seeBurgerModal = document.querySelector('.menu-items');
+    seeBurgerModal.classList.add('fs');
+    const seeBurgerCross = document.querySelector('.menu-bg');
+    seeBurgerCross.classList.add('fs');
+  },
+
+  handleCloseMenu: function(){
+    const seeMenuBurger = document.querySelector('.menu-burger');
+    seeMenuBurger.classList.remove('fs');
+    const seeBurgerModal = document.querySelector('.menu-items');
+    seeBurgerModal.classList.remove('fs');
+    const seeBurgerCross = document.querySelector('.menu-bg');
+    seeBurgerCross.classList.remove('fs');
   }
 };
 
