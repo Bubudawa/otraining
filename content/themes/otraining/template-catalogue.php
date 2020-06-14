a7f79400f845f2436f36edc41b38bd6840acf3ac
Bubudawa <sylvainbuignet@gmail.com>
2020-06-14 09:32:25 +0200

    selected on drop-down categories

--- content/themes/otraining/template-catalogue.php
+++ content/themes/otraining/template-catalogue.php
@@ -4,12 +4,12 @@ Template Name: Catalogue
 */
 ?>
 <?php get_header(); ?>
-<label class="selector__categorie"for="categories">
-    Choisissez une catégorie:</label>
-<form method="POST">
-<select class="selector__selector" style="width:12%;"name="categories" id="list">
-<option></option>
 
+<label class="selector__categorie"for="categories">Choisissez une catégorie:</label>
+<form method="POST">
+<select class="selector__selector" style="width:16rem;"name="categories" id="list">
+<!-- Toutes doit retourner une valeur vide pour afficher toutes les formations -->
+<option value="">- Toutes -</option> 
 
 <?php
 $args = [
@@ -18,25 +18,46 @@ $args = [
   'order' => 'ASC',
   'orderby' => 'title'
 ];
-$wpqueryTraining = new WP_Query($args);
-if ($wpqueryTraining->have_posts()): while ($wpqueryTraining->have_posts()): $wpqueryTraining->the_post();
-    get_template_part('template-parts/catalogue/categories');
-    endwhile;
-endif;
-?>
-</select>
-<input class="selector__button" type="submit" name="submit" value="Appliquer"/>
-</form>
-<?php 
+
 $getcat= '';
+
 if(isset($_POST['submit']))
 {
     $getcat=$_POST['categories'];
 }
+
+//Boucle pour afficher les catégories dans un drop-down
+$wpqueryCat = new WP_Query($args);
+if ($wpqueryCat->have_posts()): while ($wpqueryCat->have_posts()): $wpqueryCat->the_post();
+
+
+  $title = get_the_title();
+
+//permet d'ajouter 'selected' à la catégorie dropdown sélectionner après le submit.
+  if($getcat == $title) {
+      $selected = 'selected';
+  }
+  else {
+      $selected = '';
+  }
+  ?>
+
+  <option <?php echo $selected ?>><?php the_title(); ?></option>
+
+  <?php
+  endwhile;
+endif;
+
 ?>
-          <div class="cards">
+
+</select>
+<input class="selector__button" type="submit" name="submit" value="Appliquer"/>
+</form>
+
+<div class="cards">
 
 <?php
+
 $args = [
   'post_type' => 'formation',
   'order' => 'ASC',
@@ -44,12 +65,15 @@ $args = [
   'meta_key' => 'categorie',
   'meta_value' => $getcat
 ];
+
 $wpqueryTraining = new WP_Query($args);
 if ($wpqueryTraining->have_posts()): while ($wpqueryTraining->have_posts()): $wpqueryTraining->the_post();
+
     get_template_part('template-parts/training');
-    endwhile;
+  endwhile;
 endif;
 ?>
+
 </div>
 
 <?php get_footer(); ?>
